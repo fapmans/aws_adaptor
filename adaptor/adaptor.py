803ef9fda4feb6026837a20b898443f184afff8c
Ioram Sette <ioram7@gmail.com>
2016-01-29 23:56:08 -0300

    Fixed Adaptor to return empty policies

--- adaptor/adaptor.py
+++ adaptor/adaptor.py
@@ -5,12 +5,16 @@ import json
 def policy2dnf(policy, tenant, apf):
     resp = {}
     dnfpol = aws_parser.policy2dnf(policy)
-    resp['dnf_policy'] = semantic_parser.semantic2ontology(dnfpol, tenant, apf)
+    ontpol = semantic_parser.semantic2ontology(dnfpol, tenant, apf)
+    if ontpol:
+	    resp['dnf_policy'] = ontpol
     return resp
 
 def policy2local(dnf_policy, tenant, apf):
     resp = {}
     locpol = semantic_parser.semantic2local(dnf_policy, tenant, apf)
-    resp['policy'] = aws_parser.policy2local(locpol)
-#    resp['policy'] = locpol
+    parsedpol = aws_parser.policy2local(locpol)
+    if parsedpol:
+	    resp['policy'] = parsedpol
+    # resp['policy'] = locpol
     return resp
